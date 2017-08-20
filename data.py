eb4eb7e294481f2452358acf89022ee5e5c4fe04
Krissanah Chinsoi <torres9lion@gmail.com>
2017-08-20 16:33:45 +0700

    fix index

--- data.py
+++ data.py
@@ -1,14 +1,14 @@
 data = {
-    'name': 'James Tiberius Kirk',
-    'nickname': 'Jim',
-    'image': 'kirk.jpg',
+    'name': 'Krissanah Chinsoi',
+    'nickname': 'MAIHOM',
+    'image': 'maihom.jpg',
     'greeting_msg': 'Hi, there!',
     'about': """
-        James Tiberius "Jim" Kirk was a male Human Starfleet officer in the 23rd century. He was arguably one of the most famous and highly decorated starship captains in the history of Starfleet. As the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, Kirk served United Federation of Planets interests as an explorer, soldier, time-traveler, and diplomat.
+        Maihom is a student@ce kmitl. 1999 AUG 5. Maihom can do everything if maihom understand.
     """,
     'work': {
-        'position': 'Starfleet officer',
-        'at': 'Starfleet'
+        'position': '1D Student',
+        'at': 'kmitl'
     },
     'courses': [
         {
